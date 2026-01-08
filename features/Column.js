5ae3172a60e5d7dab26b24fb388eae4a3bc676f9
beyons <69459996+beyons@users.noreply.github.com>
2026-01-08 15:59:37 +0100

    Implement Column layout component

---
+++ features/Column.js
@@ -0,0 +1,37 @@
+import LayoutComponent from '../core/LayoutComponent.js';
+
+class Column extends LayoutComponent {
+  constructor(framework, options = {}) {
+    super(framework, options);
+    this.align = options.align || 'start'; 
+    // start | center | end | stretch
+  }
+
+  layout() {
+    let y = this.y;
+    let maxWidth = 0;
+
+    for (const child of this.children) {
+      child.y = y;
+
+      if (this.align === 'center') {
+        child.x = this.x + (this.width - child.width) / 2;
+      } else if (this.align === 'end') {
+        child.x = this.x + this.width - child.width;
+      } else if (this.align === 'stretch') {
+        child.x = this.x;
+        child.width = this.width;
+      } else {
+        child.x = this.x;
+      }
+
+      y += child.height + this.spacing;
+      maxWidth = Math.max(maxWidth, child.width);
+    }
+
+    this.height = y - this.y - this.spacing;
+    if (!this.width) this.width = maxWidth;
+  }
+}
+
+export default Column;
