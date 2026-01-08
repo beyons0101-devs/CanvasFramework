7960c72b4e054484c3f16a8a11146ac49e5c2ad9
beyons <69459996+beyons@users.noreply.github.com>
2026-01-08 15:59:59 +0100

    Add Row component for layout management

---
+++ features/Row.js
@@ -0,0 +1,33 @@
+import LayoutComponent from '../core/LayoutComponent.js';
+
+class Row extends LayoutComponent {
+  constructor(framework, options = {}) {
+    super(framework, options);
+    this.align = options.align || 'start';
+  }
+
+  layout() {
+    let x = this.x;
+    let maxHeight = 0;
+
+    for (const child of this.children) {
+      child.x = x;
+
+      if (this.align === 'center') {
+        child.y = this.y + (this.height - child.height) / 2;
+      } else if (this.align === 'end') {
+        child.y = this.y + this.height - child.height;
+      } else {
+        child.y = this.y;
+      }
+
+      x += child.width + this.spacing;
+      maxHeight = Math.max(maxHeight, child.height);
+    }
+
+    this.width = x - this.x - this.spacing;
+    if (!this.height) this.height = maxHeight;
+  }
+}
+
+export default Row;
