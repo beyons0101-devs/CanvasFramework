9bbc0b8eafae80bf176fc8cdca7cdd86cc732872
beyons <69459996+beyons@users.noreply.github.com>
2026-01-09 10:04:29 +0100

    Update Row.js

--- features/Row.js
+++ features/Row.js
@@ -1,30 +1,22 @@
-import LayoutComponent, { makeConstraints } from './LayoutComponent.js';
+import LayoutComponent from './LayoutComponent.js';
 
 class Row extends LayoutComponent {
   constructor(framework, options = {}) {
     super(framework, options);
-    this.align = options.align || 'start'; // start | center | end | stretch
+    this.align = options.align || 'start'; // start | center | end
   }
 
   layout() {
-    const constraints = makeConstraints(this);
     let x = this.x;
     let maxHeight = 0;
 
     for (const child of this.children) {
-      const childSize = child.measure(constraints);
-      child.width = childSize.width;
-      child.height = childSize.height;
-
       child.x = x;
 
       if (this.align === 'center') {
         child.y = this.y + (this.height - child.height) / 2;
       } else if (this.align === 'end') {
         child.y = this.y + this.height - child.height;
-      } else if (this.align === 'stretch') {
-        child.y = this.y;
-        child.height = this.height;
       } else {
         child.y = this.y;
       }
@@ -35,6 +27,13 @@ class Row extends LayoutComponent {
 
     this.width = x - this.x - this.spacing;
     if (!this.height) this.height = maxHeight;
+
+    // Layout récursif automatique des enfants
+    for (const child of this.children) {
+      if (typeof child.layoutRecursive === 'function') {
+        child.layoutRecursive();
+      }
+    }
   }
 }
 
